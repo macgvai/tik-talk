c016f803d68b72d40628579d0ad45b1d6d9d3c69
Victor Stupitsyn <70962976+macgvai@users.noreply.github.com>
2025-04-02 17:00:08 +0300

    Merge pull request #1
    * side-bar

---
+++ src/app/common-ui/side-bar/interfaces/menu-button.interface.ts
@@ -0,0 +1,4 @@
+export interface MenuButtonInterface {
+    name: string,
+    icon: string
+}
--- src/app/common-ui/side-bar/side-bar.component.ts
+++ src/app/common-ui/side-bar/side-bar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {SvgImgComponent} from '../svg-img/svg-img.component';
+import {NgClass} from '@angular/common';
+import {MenuButtonInterface} from './interfaces/menu-button.interface';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [],
+    imports: [
+        SvgImgComponent,
+        NgClass
+    ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+    menuButtons: MenuButtonInterface[] = [
+        {
+            name: 'Моя страница',
+            icon: 'telegram'
+        },
+        {
+            name: 'Моя страница2',
+            icon: 'eye'
+        },
+        {
+            name: 'Моя страница3',
+            icon: 'telegram'
+        }
+    ]
+
 
 }
